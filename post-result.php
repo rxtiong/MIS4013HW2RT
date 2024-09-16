4170798cc90b7868e38b5bc67a65d3e2a2628afb
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-09-16 00:39:01 -0500

    Create post-result.php

---
+++ post-result.php
@@ -0,0 +1,20 @@
+<?php 
+$pageTitle = "Post result";
+include "view-header.php";
+?>
+    <h1>Post result</h1>
+<?php 
+if (isset($_POST['my-name'])) {
+  ?>
+  <p>The value sent is:</p>
+  <?php
+  echo $_POST['my-name'];
+}
+else {
+  ?>
+  <p>Nothing posted to the page.</p>
+  <?php
+}
+
+include "view-footer.php";
+?>
